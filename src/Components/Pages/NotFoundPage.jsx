7b4faf8f5d23d8d7e180b555904b5d36aba02acc
amruta <amrutadeokate50@gmail.com>
2022-09-16 11:49:30 +0530

    amruta-404-page

---
+++ src/Components/Pages/NotFoundPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const NotFoundPage = () => {
+    return (
+        <>
+            <div class="container">
+                <div class="row-md-6 lg-12 sm-3">
+                    <div class="img1 bg-no-repeat bg-center bg-auto h-screen w-full"
+                        style="background-image: url('./public/images/Group_2.svg');"></div>
+                    <h1 class="text">#Scanbills</h1>
+                    <div class="img2 bg-no-repeat bg-center bg-auto h-screen w-full"
+                        style="background-image: url('./public/images/Group_1.svg');">
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+export default NotFoundPage
