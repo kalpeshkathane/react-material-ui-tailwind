61496d6343f5cb00bdb8c8120f3ba927cc6c890b
amruta <amrutadeokate50@gmail.com>
2022-09-27 22:17:13 +0530

    404-page-amruta

--- src/Components/Pages/NotFoundPage.jsx
+++ src/Components/Pages/NotFoundPage.jsx
@@ -3,19 +3,15 @@ import Group1 from '../../Assets/images/Group_1.svg'
 import Group2 from '../../Assets/images/Group_2.svg'
 const NotFoundPage = () => {
     return (
-    
-       <>
-       <div>
-            <div class="relative">
-                <div className='w-full  md:w-full lg:w-full'><img src={Group2} alt="" /></div>
-                <div class="flex">
-                    <h1 className='absolute bottom-16 left-0 lg:text-6xl md:text-4xl md:bottom-32 font-bold text-orange-400 '>#Scanbills</h1>
-                    <div className='absolute top-10 right-0 w-28 md:w-64 md:top-24 lg:w-96 lg:top-24 lg:right-0'><img src={Group1} alt="" />
-                    </div>
+        <div className="max-w-4xl flex flex-col items-center justify-center bg-gray-500">
+            <div className="relative bg-slate-50">
+                <img src={Group2} alt="" />
+                <div className="flex items-center justify-center absolute w-full top-5 p-5">
+                    <div className="w-1/2"> <h1 className='font-bold text-xl md:text-4xl lg:text-5xl text-orange-400 '>#Scanbills</h1></div>
+                    <div className='w-1/2'><img src={Group1} alt="" /></div>
                 </div>
             </div>
-            </div>
-        </>
+        </div>
     )
 
 }
