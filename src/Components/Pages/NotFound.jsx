283a4fa435e48688d643068e90dd8b00d76657f0
SNEHALHARAGAPURE <103924157+SNEHALHARAGAPURE@users.noreply.github.com>
2022-09-28 12:45:12 +0530

    not found page

--- src/Components/Pages/NotFound.jsx
+++ src/Components/Pages/NotFound.jsx
@@ -5,17 +5,16 @@ import Group2 from "../../Assets/images/Group_2.svg"
 const NotFound = () => {
     return (
 
-        <>
-            <div className="relative">
-                <div className='w-full  md:w-full lg:w-full'><img src={Group2} alt="" /></div>
-                <div class="flex">
-                    <h1 className='absolute bottom-16 left-0 lg:text-6xl md:text-4xl md:bottom-32 font-bold text-orange-400 '>#Scanbills</h1>
-                    <div className='absolute top-10 right-0 w-32 md:w-64 md:top-24 lg:w-96 lg:top-24 lg:right-0'><img src={Group1} alt="" />
+        <div className='w-full h-full flex justify-center items-center bg-white'>
+            <div className='absolute'>
+                <img src={Group2} alt="" />
+                <div className='flex items-center justify-center w-full '>
+                    <h1 className='absolute text-6xl font-bold text-orange-400 top-96 left-52'>#Scanbills</h1>
+                    <div className='absolute left-1/2 top-24 right-0'><img src={Group1} alt="" />
                     </div>
                 </div>
             </div>
-        </>
-
+        </div>
     )
 }
 
